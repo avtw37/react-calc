c39ff2a03fa6856cdb8ee588604c89d115ae2afa
Alexis Dimas <alexisdimas@Alexiss-MacBook-Pro.local>
2019-06-25 11:10:29 -0700

    Updates

---
+++ precache-manifest.05a32134f2ab5c2d3da3aed7aeac5187.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5ef9f553f845610ebd80b9cf7ce204c",
+    "url": "/react-calc/index.html"
+  },
+  {
+    "revision": "7e6e57872a8a3df50b5d",
+    "url": "/react-calc/static/css/main.d9c2c851.chunk.css"
+  },
+  {
+    "revision": "0a052bb7d76e5c6c8a69",
+    "url": "/react-calc/static/js/2.5cd37059.chunk.js"
+  },
+  {
+    "revision": "7e6e57872a8a3df50b5d",
+    "url": "/react-calc/static/js/main.6f86de27.chunk.js"
+  },
+  {
+    "revision": "48eedb427e7cd8bd6dd1",
+    "url": "/react-calc/static/js/runtime~main.0d723d41.js"
+  }
+]);
